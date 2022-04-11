d21d86b6ecdbdd9fc4270178800ddd5abd77359c
mousa ageeli <mousaageeli@gmail.com>
2022-04-11 23:43:03 +0300

    start gaol project

--- the_know/settings.py
+++ the_know/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'the_know.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/questions/templates'],
+        'DIRS': ['/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
