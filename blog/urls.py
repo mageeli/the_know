40bcc61aa4e1fdd79ee2c647e4095756b06249a8
mousa ageeli <mousaageeli@gmail.com>
2022-04-29 08:25:02 +0300

    new design

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from  django.urls import re_path, include
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    re_path(r'^posts/$', views.PostView.as_view(), name='posts'),
-    re_path(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='post'),
-    re_path(r'^addPost/$', views.WriteView.as_view(success_url="blog/posts/"), name='addPost')
+    re_path(r'^posts/$', login_required(views.PostView.as_view()), name='posts'),
+    re_path(r'^post/(?P<pk>[0-9]+)/$', login_required(views.PostDetailView.as_view()), name='post'),
+    re_path(r'^addPost/$', login_required(views.WriteView.as_view(success_url="/blog/posts/")), name='addPost')
 ]
--- questions/urls.py
+++ questions/urls.py
@@ -1,11 +1,12 @@
 from  django.urls import re_path, include
 from questions.views import HomePageView, QuestionsView, QuestionDetailView, AddQuestionView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    re_path(r'^$', HomePageView, name='home'),
-    re_path(r'^questions/$', QuestionsView.as_view(), name='questions'),
-    re_path(r'^question/(?P<pk>[0-9]+)/$', QuestionDetailView.as_view(), name='question'),
-    re_path(r'^addQuestion/$', AddQuestionView.as_view(success_url="/questions/"), name='addQuestion'),
+    re_path(r'^$', login_required(HomePageView), name='home'),
+    re_path(r'^questions/$', login_required(QuestionsView.as_view()), name='questions'),
+    re_path(r'^question/(?P<pk>[0-9]+)/$', login_required(QuestionDetailView.as_view()), name='question'),
+    re_path(r'^addQuestion/$', login_required(AddQuestionView.as_view(success_url="/questions/")), name='addQuestion'),
     re_path('accounts/', include('django.contrib.auth.urls')),
 
               ]
