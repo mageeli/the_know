8f550d51f41ce2659a7c2c9363d63d7f2fddb86c
mousa ageeli <mousaageeli@gmail.com>
2022-05-14 17:48:41 +0300

    task patch 1

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,10 +3,16 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    re_path(r'^posts/$', login_required(views.PostView.as_view()), name='posts'),
+    re_path(r'^lists/$', login_required(views.ListListView.as_view()), name='index'),
+    re_path(r'^list/(?P<pk>[0-9]+)/$', login_required(views.ItemListView.as_view()), name='list'),
+    re_path(r'^list/(?P<pk>[0-9]+)/delete/$', login_required(views.ListDelete.as_view()), name='list-delete'),
+    re_path(r'^lists/add/$', login_required(views.ListCreate.as_view(success_url="/tasks/lists/")), name='list-add'),
+    re_path(r'^list/(?P<pk>[0-9]+)/item/add/$', login_required(views.ItemCreate.as_view()), name='item-add'),
+    re_path(r'^list/(?P<list_id>[0-9]+)/item/(?P<pk>[0-9]+)/delete/$', login_required(views.ItemDelete.as_view()), name='item-delete'),
+
     # re_path(r'^post/(?P<pk>[0-9]+)/$', login_required(views.PostDetailView.as_view()), name='post'),
-    # re_path(r'^post/(?P<pk>[0-9]+)/edit$', login_required(views.UpdateView.as_view()), name='editPost'),
-    # re_path(r'^addPost/$', login_required(views.WriteView.as_view(success_url="/blog/posts/")), name='addPost')
+    re_path(r'^list/(?P<list_id>[0-9]+)/item/(?P<pk>[0-9]+)/update$', login_required(views.ItemUpdate.as_view()), name='item-update'),
+
 ]
 # urlpatterns = [
 #     re_path(r'', login_required(views.ListListView.as_view()), name='index'),
--- tasks/views.py
+++ tasks/views.py
@@ -13,20 +13,26 @@ class ListListView(ListView):
     model = ToDoList
     template_name = "tasks/tasks.html"
 
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["todo_list_items"] = ToDoItem.objects.all()
+        return context
+
 class ItemListView(ListView):
     model = ToDoItem
     template_name = "tasks/tasks_list.html"
 
     def get_queryset(self):
-        return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs["pk"])
 
     def get_context_data(self):
         context = super().get_context_data()
-        context["todo_list"] = ToDoList.objects.get(id=self.kwargs["list_id"])
+        context["todo_list"] = ToDoList.objects.get(id=self.kwargs["pk"])
         return context
 
 class ListCreate(CreateView):
     model = ToDoList
+    template_name = "tasks/add_task_list.html"
     fields = ["title"]
 
     def get_context_data(self):
@@ -36,6 +42,7 @@ class ListCreate(CreateView):
 
 class ItemCreate(CreateView):
     model = ToDoItem
+    template_name = "tasks/add_task_item.html"
     fields = [
         "todo_list",
         "title",
@@ -46,13 +53,13 @@ class ItemCreate(CreateView):
 
     def get_initial(self):
         initial_data = super(ItemCreate, self).get_initial()
-        todo_list = ToDoList.objects.get(id=self.kwargs["list_id"])
+        todo_list = ToDoList.objects.get(id=self.kwargs["pk"])
         initial_data["todo_list"] = todo_list
         return initial_data
 
     def get_context_data(self):
         context = super(ItemCreate, self).get_context_data()
-        todo_list = ToDoList.objects.get(id=self.kwargs["list_id"])
+        todo_list = ToDoList.objects.get(id=self.kwargs["pk"])
         context["todo_list"] = todo_list
         context["title"] = "Create a new item"
         return context
@@ -62,6 +69,7 @@ class ItemCreate(CreateView):
 
 class ItemUpdate(UpdateView):
     model = ToDoItem
+    template_name = "tasks/add_task_item.html"
     fields = [
         "todo_list",
         "title",
@@ -82,12 +90,14 @@ class ItemUpdate(UpdateView):
 
 class ListDelete(DeleteView):
     model = ToDoList
+    template_name = "tasks/task_list_confirm_delete.html"
     # You have to use reverse_lazy() instead of reverse(),
     # as the urls are not loaded when the file is imported.
     success_url = reverse_lazy("index")
 
 class ItemDelete(DeleteView):
     model = ToDoItem
+    template_name = "tasks/task_item_confirm_delete.html"
 
     def get_success_url(self):
         return reverse_lazy("list", args=[self.kwargs["list_id"]])
@@ -98,6 +108,3 @@ class ItemDelete(DeleteView):
         return context
 
 
-class PostView(ListView):
-    model = ToDoItem
-    template_name = "tasks/tasks_list.html"
